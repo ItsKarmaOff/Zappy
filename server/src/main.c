5d14d2b5b693904883cf18bbf8e67dce37d51504
Nicolas TORO <nicolas.toro@epitech.eu>
2025-05-29 22:37:05 +0200

    FEAT: add documentation to main and replace the main of the server by a valid main

--- server/src/main.c
+++ server/src/main.c
@@ -2,18 +2,20 @@
 ** EPITECH PROJECT, 2025
 ** Zappy
 ** File description:
-** The main function of the ai
+** The main function of the server
 */
 /**
- * @file Main.cpp
- * @brief The main function of the ai
+ * @file main.c
+ * @brief The main function of the server
  * @author Christophe VANDEVOIR, Gianni TUERO, Lou PELLEGRINO,
  * Nicolas TORO, Olivier POUECH and Raphael LAUNAY
  */
 
-#include "Lib.hpp"
+#include "my.h"
 
 int main(UNUSED int argc, UNUSED char **argv)
 {
+    my_update_malloc(TRUE);
+    my_free();
     return 0;
 }
