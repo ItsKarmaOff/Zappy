7c21aba408aa746df4145439fdc2e65d6a487b6f
Nicolas TORO <nicolas.toro@epitech.eu>
2025-06-10 13:35:11 +0200

    FEAT: implement msz gui command in the server

--- server/src/commands/to_gui/msz.c
+++ server/src/commands/to_gui/msz.c
@@ -14,5 +14,21 @@
 
 void send_msz_to_gui(server_t *server, UNUSED client_t *client)
 {
-    return;
+    if (server == NULL)
+        return;
+    if (client != NULL) {
+        dprintf(client->socket_fd,
+            "msz %zu %zu\n",
+            server->game.game_settings.width,
+            server->game.game_settings.height);
+        return;
+    }
+    for (size_t index = 0; index < server->current_clients_number; index++) {
+        if (server->client_list[index]->is_gui) {
+            dprintf(server->client_list[index]->socket_fd,
+                "msz %zu %zu\n",
+                server->game.game_settings.width,
+                server->game.game_settings.height);
+        }
+    }
 }
