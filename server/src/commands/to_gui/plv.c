1fa75febd1df0c7f39feadabc2a0328d29cdaa6d
Nicolas TORO <nicolas.toro@epitech.eu>
2025-06-10 15:29:21 +0200

    FEAT: implement plv gui command in the server

--- server/src/commands/to_gui/plv.c
+++ server/src/commands/to_gui/plv.c
@@ -15,5 +15,15 @@
 void send_plv_to_gui(UNUSED server_t *server, UNUSED client_t *client,
     UNUSED player_t *player, UNUSED size_t level)
 {
-    return;
+    if (server == NULL || player == NULL)
+        return;
+    if (client != NULL) {
+        dprintf(client->socket_fd, "plv #%zu %zu\n", player->id, level);
+        return;
+    }
+    for (size_t index = 0; index < server->current_clients_number; index++) {
+        if (server->client_list[index]->is_gui)
+            dprintf(server->client_list[index]->socket_fd,
+                "plv #%zu %zu\n", player->id, level);
+    }
 }
