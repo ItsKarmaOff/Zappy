11d296e478254e2230a12408fa818ba02908a162
Nicolas TORO <nicolas.toro@epitech.eu>
2025-06-10 23:33:54 +0200

    FIX: change the enw command to match with the reference binary

--- server/src/commands/to_gui/enw.c
+++ server/src/commands/to_gui/enw.c
@@ -18,15 +18,18 @@ void send_enw_to_gui(server_t *server, client_t *client,
     if (server == NULL || egg == NULL)
         return;
     if (client != NULL) {
-        dprintf(client->socket_fd, "enw #%zu #%zu %zu %zu\n",
-            egg->id, egg->creator_id, egg->position.x, egg->position.y);
+        dprintf(client->socket_fd, "enw #%zu #%zi %zu %zu\n",
+            egg->id, (egg->creator_id == 0) ? -1 : (ssize_t)egg->creator_id,
+            egg->position.x, egg->position.y);
         return;
     }
     for (size_t index = 0; index < server->current_clients_number; index++) {
         if (server->client_list[index]->is_gui) {
             dprintf(server->client_list[index]->socket_fd,
-                "enw #%zu #%zu %zu %zu\n",
-                egg->id, egg->creator_id, egg->position.x, egg->position.y);
+                "enw #%zu #%zi %zu %zu\n",
+                egg->id,
+                (egg->creator_id == 0) ? -1 : (ssize_t)egg->creator_id,
+                egg->position.x, egg->position.y);
         }
     }
 }
