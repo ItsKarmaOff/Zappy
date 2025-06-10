c96fad002b9a0cbb84964f4b499b5590f3e7d64a
Nicolas TORO <nicolas.toro@epitech.eu>
2025-06-10 13:43:39 +0200

    FEAT: implement smg gui command in the server

---
+++ server/src/commands/to_gui/smg.c
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2025
+** zappy
+** File description:
+** The smg.c
+*/
+/**
+ * @file smg.c
+ * @brief The smg.c
+ * @author Nicolas TORO
+ */
+
+#include "commands/gui.h"
+
+void send_smg_to_gui(server_t *server, client_t *client,
+    const char *message)
+{
+    if (server == NULL || message == NULL)
+        return;
+    if (client != NULL) {
+        dprintf(client->socket_fd, "smg %s\n", message);
+        return;
+    }
+    for (size_t index = 0; index < server->current_clients_number; index++) {
+        if (server->client_list[index]->is_gui)
+            dprintf(server->client_list[index]->socket_fd,
+                "smg %s\n", message);
+    }
+}
