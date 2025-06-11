d067cf27077b88f5d8a769a09ca9962accb24564
Nicolas TORO <nicolas.toro@epitech.eu>
2025-06-11 15:32:36 +0200

    FIX: fix some little bugs in the libmy C

--- libs/my_c/include/my/functions.h
+++ libs/my_c/include/my/functions.h
@@ -235,6 +235,7 @@ bool my_char_is_printable(char c);
 
 /**
  * @brief Get the index of a char in a str according to its occurrence
+ * @note The first occurrence is 1, the second is 2, etc.
  * @param str The string to check
  * @param c The letter to find
  * @param occurrence The occurrence of the letter to find
