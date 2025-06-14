1dda16df0332e07ea4f82f3f1211fadb92323e75
Nicolas TORO <nicolas.toro@epitech.eu>
2025-06-14 20:55:17 +0200

    FEAT: implement the inventory internal server commands

--- server/src/commands/from_server/inventory.c
+++ server/src/commands/from_server/inventory.c
@@ -12,8 +12,51 @@
 
 #include "commands/commands_server.h"
 
-void handle_server_command_inventory(
-    UNUSED server_t *server, UNUSED client_t *client, UNUSED char **args)
+bool check_resource_args(
+    server_t *server, char **args, size_t *ressource_id, size_t *quantity)
 {
+    *ressource_id = my_get_number(args[2], DEFAULT_NB);
+    if (my_errno != SUCCESS || *ressource_id >= RESOURCES_SIZE) {
+        ERROR(my_create_str("Invalid resource ID: %s\n", args[2]));
+        return false;
+    }
+    *quantity = my_get_number(args[3], DEFAULT_NB);
+    if (my_errno != SUCCESS) {
+        ERROR(my_create_str("Invalid quantity: %s\n", args[3]));
+        return false;
+    }
+    return true;
+}
+
+static void update_inventory(
+    server_t *server, player_t *player, size_t ressource_id, size_t quantity)
+{
+    player->inventory[ressource_id] += quantity;
+    send_pin_to_gui(server, NULL, player);
+    printf("Player #%zu's set %s x%zu\n",
+        player->id, resources_names[ressource_id], quantity);
+}
+
+void handle_server_command_inventory(
+    server_t *server, UNUSED client_t *client, char **args)
+{
+    size_t player_id = 0;
+    size_t ressource_id = 0;
+    size_t quantity = 0;
+
     DEBUG("Executing \"Inventory\" command\n");
+    if (my_array_len((void **)args) != 4) {
+        ERROR("Invalid number of arguments for \"Inventory\" command\n");
+        return;
+    }
+    player_id = my_get_number(args[1], DEFAULT_NB);
+    if (my_errno != SUCCESS || player_id == 0
+    || player_id >= server->game.game_settings.next_player_id) {
+        ERROR(my_create_str("Invalid player ID: %s\n", args[1]));
+        return;
+    }
+    if (!check_resource_args(server, args, &ressource_id, &quantity))
+        return;
+    update_inventory(server, get_player_by_id(&server->game, player_id),
+        ressource_id, quantity);
 }
