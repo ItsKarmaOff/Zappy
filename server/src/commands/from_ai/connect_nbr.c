f52ddf79107bb3a655d9e279d2a366392fc36f7b
DimitriLaPoudre <lou.pellegrino@epitech.eu>
2025-06-16 08:42:24 +0200

    STYLE:

--- server/src/commands/from_ai/connect_nbr.c
+++ server/src/commands/from_ai/connect_nbr.c
@@ -16,5 +16,9 @@ void handle_command_connect_nbr(UNUSED server_t *server,
     UNUSED client_t *client, UNUSED char **args)
 {
     DEBUG("Executing \"Connect_nbr\" command\n");
+    if (my_array_len((void **) args) != 1) {
+        dprintf(client->socket_fd, WRONG_AI);
+        return;
+    }
     dprintf(client->socket_fd, "%zu\n", client->player->team->eggs_number);
 }
--- server/src/commands/from_ai/eject.c
+++ server/src/commands/from_ai/eject.c
@@ -15,7 +15,7 @@
 static void destroy_egg(tile_t **map, player_t *egg)
 {
     egg->team->eggs_number--;
-    my_free_ptr(my_pop_node(&(ACCESS_MAP(map,egg).clients), egg, NULL));
+    my_free_ptr(my_pop_node(&(ACCESS_MAP(map, egg).clients), egg, NULL));
     my_free_ptr(my_pop_node(&egg->team->player_list, egg, NULL));
     my_free_ptr(egg);
 }
@@ -25,8 +25,6 @@ static void move_player(game_t *game, player_t *player,
 {
     my_free_ptr(my_pop_node(&(ACCESS_MAP(game->map, player).clients),
     player, NULL));
-    printf("x: %zu y: %zu\n", player->position.x, player->position.y);
-    printf("dx: %ld dy: %ld\n", !((long)orientation % 2) * (3 - (long)orientation), ((long)orientation % 2) * ((long)orientation - 2));
     player->position.x = ((long)player->position.x +
         game->game_settings.width + !((long)orientation % 2)
         * (3 - (long)orientation)) % game->game_settings.width;
@@ -34,17 +32,15 @@ static void move_player(game_t *game, player_t *player,
         game->game_settings.height + ((long)orientation % 2)
         * ((long)orientation - 2)) % game->game_settings.height;
     my_push_back(&(ACCESS_MAP(game->map, player).clients), player, UNKNOWN);
-    printf("x: %zu y: %zu\n", player->position.x, player->position.y);
-    printf("ejecter: %u ejected: %u\n", orientation, player->orientation);
     if ((long)orientation - (long)player->orientation == 0)
         dprintf(player->client->socket_fd, "eject: %d\n", 5);
-    else if ((long)orientation - (long)player->orientation == 2
+    if ((long)orientation - (long)player->orientation == 2
     || (long)player->orientation - (long)orientation == 2)
         dprintf(player->client->socket_fd, "eject: %d\n", 1);
-    else if ((long)orientation - (long)player->orientation == 1
+    if ((long)orientation - (long)player->orientation == 1
     || (long)player->orientation - (long)orientation == 1)
         dprintf(player->client->socket_fd, "eject: %d\n", 3);
-    else if ((long)orientation - (long)player->orientation == 3
+    if ((long)orientation - (long)player->orientation == 3
     || (long)player->orientation - (long)orientation == 3)
         dprintf(player->client->socket_fd, "eject: %d\n", 7);
 }
@@ -64,7 +60,8 @@ void handle_command_eject(UNUSED server_t *server, UNUSED client_t *client,
         if (((player_t *)tmp->data)->is_egg) {
             destroy_egg(server->game.map, tmp->data);
             fail = false;
-        } else if (tmp->data != client->player) {
+        }
+        if (!((player_t *)tmp->data)->is_egg && tmp->data != client->player) {
             move_player(&server->game, tmp->data,
                 client->player->orientation);
             fail = false;
