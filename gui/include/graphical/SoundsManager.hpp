19327728459996e6a474b174c92911d68b27c7a0
Nicolas TORO <nicolas.toro@epitech.eu>
2025-06-28 21:44:22 +0200

    STYLE: clean gui file: SoundsManager.hpp

--- gui/include/graphical/SoundsManager.hpp
+++ gui/include/graphical/SoundsManager.hpp
@@ -1,17 +1,21 @@
 /*
 ** EPITECH PROJECT, 2025
-** SoundsManager.hpp
+** Zappy
 ** File description:
-** SoundsManager declaration
+** The SoundsManager class declaration
 */
-
+/**
+ * @file SoundsManager.hpp
+ * @brief The SoundsManager class declaration
+ * @author Christophe VANDEVOIR, Gianni TUERO, Lou PELLEGRINO,
+ * Nicolas TORO, Olivier POUECH and Raphael LAUNAY
+ */
 
 #ifndef SOUNDSMANAGER_HPP
     #define SOUNDSMANAGER_HPP
 
-
+    #include "Lib.hpp"
     #include <raylib.h>
-#include <unordered_map>
 
 namespace Gui {
     class SoundsManager {
@@ -38,7 +42,6 @@ namespace Gui {
         private:
             std::unordered_map<SongType, Sound> _sounds;
             std::unordered_map<MusicType, Music> _musics;
-
     };
 }
 
