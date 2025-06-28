8004d2ce77228b6125849f1b016e2a79f58ec8de
Nicolas TORO <nicolas.toro@epitech.eu>
2025-06-28 19:01:09 +0200

    STYLE: clean gui file: Chatbox.hpp

--- gui/include/graphical/Chatbox.hpp
+++ gui/include/graphical/Chatbox.hpp
@@ -1,37 +1,36 @@
 /*
 ** EPITECH PROJECT, 2025
-** Chatbox.hpp
+** Zappy
 ** File description:
-** Chatbox declaration
+** The Chatbox class declaration
 */
-
+/**
+ * @file Chatbox.hpp
+ * @brief The Chatbox class declaration
+ * @author Christophe VANDEVOIR, Gianni TUERO, Lou PELLEGRINO,
+ * Nicolas TORO, Olivier POUECH and Raphael LAUNAY
+ */
 
 #ifndef CHATBOX_HPP
     #define CHATBOX_HPP
 
-#include <queue>
-#include <raylib.h>
-#include <string>
-#include <chrono>
-
-/**
- * @struct message
- * @brief Represents a message in the chatbox with content, sender information, timestamp, and color
- * @author Gianni TUERO
- */
-typedef struct message {
-    std::string content;                         ///< The content of the message
-    std::string sender;                          ///< The sender of the message
-    std::chrono::steady_clock::time_point timestamp; ///< When the message was created
-    Color color;                                 ///< The color used to display the message
-} message_t;
-
-/**
- * @file Chatbox.hpp
- * @brief The Chatbox class for managing and displaying chat messages
- */
+    #include "Lib.hpp"
+    #include <raylib.h>
 
 namespace Gui {
+    /**
+     * @struct message
+     * @brief Represents a message in the chatbox with content, sender information, timestamp, and color
+     */
+    typedef struct message {
+        std::string content; ///< The content of the message
+        std::string sender; ///< The sender of the message
+        std::chrono::steady_clock::time_point timestamp; ///< When the message was created
+        Color color; ///< The color used to display the message
+    } message_t;
+
+
+
     /**
      * @class Chatbox
      * @brief Manages the display and storage of chat messages with timed expiration
@@ -75,10 +74,15 @@ namespace Gui {
              */
             void draw(Vector2 position, Vector2 size, int fontSize = GetScreenHeight() / 30);
 
+
+
         private:
-            size_t _maxMessages;            ///< Maximum number of messages to display
+
+            //////////////// Private Attributes ////////////////////////////////
+
+            size_t _maxMessages; ///< Maximum number of messages to display
             std::deque<message_t> _messages; ///< Queue of messages
-            Font _font;                     ///< Font used for rendering messages
+            Font _font; ///< Font used for rendering messages
 
     };
 }
